c32b662cf0a4d5a7e61d4d57e523850da46a9bf8
github username <github email address>
2019-12-26 00:22:14 -0800

    pass props

--- src/index.js
+++ src/index.js
@@ -6,7 +6,24 @@ import CommentDetail from './CommentDetail'
 const App = () => {
   return (
     <div className="ui container comments">
-      <CommentDetail />
+      <CommentDetail 
+        author="Sam" 
+        timeAgo="Today at 4:45PM" 
+        content="Nice post!" 
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        author="Alex" 
+        timeAgo="Today at 2AM" 
+        content="I agree with you"  
+        avatar={faker.image.avatar()}
+      />
+      <CommentDetail 
+        author="Jane" 
+        timeAgo="Today at 5:30PM" 
+        content="I like what you said"  
+        avatar={faker.image.avatar()}
+      />
     </div>
   )
 }
